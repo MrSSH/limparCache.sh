117737471d07a255042870d3d7ec1788c950a92c
JorisBRA <joriscp@gmail.com>
2017-01-10 20:46:17 -0200

    limparCache.sh

---
+++ limparCache.sh
@@ -0,0 +1,17 @@
+# limparCache.sh
+
+cat -n /etc/issue |grep 1 |cut -d' ' -f6,7,8 |sed 's/1//' |sed 's/      //' > /etc/so
+echo -e "\033[1;31mPARA LIMPAR O CACHE DE UM ENTER!\033[0m"
+read -p "" ip
+
+
+echo "Limpando o Cache e Swap..."
+echo 3 > /proc/sys/vm/drop_caches
+sysctl -w vm.drop_caches=3
+swapoff -a && swapon -a
+clear
+
+service ssh restart & service squid3 restart
+clear
+
+echo -e "\033[01;31mLimpeza do Cache e Swap concluida\033[0m"
